354b553ab89bbb6df277fe259abe46daf25fc0c5
evasiallou <evasiallou20@gmail.com>
2023-09-04 17:24:03 +0000

    added event routes

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 from fastapi import FastAPI
 from routes.users import user_router
+from routes.event import event_router
 import uvicorn
 
 app = FastAPI()
 
 app.include_router(user_router, prefix="/user")
+app.include_router(event_router, prefix="/event")
 
 if __name__ == "__main__" :
     uvicorn.run("main:app", host ="127.0.0.1", port=8080, reload=True)
-
-
-
-
--- routes/event.py
+++ routes/event.py
@@ -1,25 +1,49 @@
-from fastapi import APIRouter, HTTPException, status
-from models.users import User, UserSignIn
+from fastapi import APIRouter, Body, HTTPException, status
+from models.event import Event
+from typing import List
 
-user_router = APIRouter(tags=["User"])
+event_router = APIRouter(
+    tags=['Event']
+)
 
-users = {}
+#our test local datastorage
+events = []
 
-@user_router.post('/signup')
-async def sign_new_user(data: User )->dict:
-    if data.email in users:
-        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="you have already been signedIn let try connect you ")
-    users[data.email]= data
-    return {
-        "message":"User successfully registred!"
-    }
 
-@user_router.post("/signin")
-async def sign_in(userauth: UserSignIn)->dict:
-    if users[userauth.email] not in users:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="user not found please create an account")
-    if users[userauth.email].password != userauth.password:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="wrong credentials please passed enter the right one"):
+#here are the routes that we use to retrieve event
+@event_router.get('/', response_model=List[Event])
+async def retrieve_all_event()->List[Event]:
+    return events
+
+@event_router.get("/{id}", response_model=Event)
+async def retrieve_event(id: int)->Event:
+    for event in events:
+        if event.id == id:
+            return event
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Event with suplied id was not found")
+
+
+#here are routes to create an event
+@event_router.post("/new_event")
+async def create_event(body: Event=Body(...))->dict:
+    events.append(body)
     return{
-        "message":"user signed in successfully"
+        "message":"Event created successfully"
     }
+#routes to delete an event
+@event_router.delete("/{id}")
+async def delete_event(id:int)->dict:
+    for event in events:
+        if event.id == id:
+            events.remove(event)
+            return {
+                "message": "Event deleted successfully"
+            }
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="event with suplied id was not found to be deleted")
+
+@event_router.delete("/")
+async def delete_all_events()-> dict:
+    events.clear()
+    return {
+        "message":"Events deleted successfully"
+    } 
